3ffecaf449ad0ab7319dccdef20ef2b5a5fc50d3
TOKUMK <jcaseyire@gmail.com>
2017-07-03 12:59:53 +0700

    add model

---
+++ FlashCardModel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+
+
+//https://developer.apple.com/library/content/referencelibrary/GettingStarted/DevelopiOSAppsSwift/
+public class FlashCardModel {
+
+    public String                       question { get; set; }
+    public Dictionary<String, Boolean>  answerSet { get; set; }
+    public String                       notes { get; set; }
+
+
+
+
+
+
+}
